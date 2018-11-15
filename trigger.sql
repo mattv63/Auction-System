5fee56ad87f97045394e7928ae4e69b79329e021
anb186 <42851849+anb186@users.noreply.github.com>
2018-11-15 12:26:48 -0500

    Add files via upload

---
+++ trigger.sql
@@ -0,0 +1,30 @@
+--trigger, fucntions, procedures, etc
+
+CREATE OR REPLACE PROCEDURE proc_putProduct;
+
+
+
+CREATE OR REPLACE TRIGGER trig_bidTimeUpdate;
+
+
+
+CREATE OR REPLACE TRIGGER trig_updateHighBid;
+
+
+
+CREATE OR REPLACE FUNCTION func_productCount; 
+
+
+
+CREATE OR REPLACE FUNCTION func_bidCount;
+
+
+
+CREATE OR REPLACE FUNCTION func_buyingAmount;
+
+
+
+CREATE OR REPLACE TRIGGER trig_closeAuctions;
+
+
+
