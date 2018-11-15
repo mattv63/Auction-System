fd0a3ee2f7feaec82f4ddd6ae2b3f5e73e3eaa18
jam395 <jam395@pitt.edu>
2018-11-15 13:02:00 -0500

    added some triggeers

--- trigger.sql
+++ trigger.sql
@@ -9,7 +9,15 @@ CREATE OR REPLACE TRIGGER trig_bidTimeUpdate;
 
 
 CREATE OR REPLACE TRIGGER trig_updateHighBid;
-
+-- to do
+after insert on bidlog
+for each row
+begin
+  update product
+  set amount = :new.amount
+  where auction_id = :new.auction_id;
+end;
+/
 
 
 CREATE OR REPLACE FUNCTION func_productCount; 
@@ -25,6 +33,14 @@ CREATE OR REPLACE FUNCTION func_buyingAmount;
 
 
 CREATE OR REPLACE TRIGGER trig_closeAuctions;
-
+-- to do
+after update of current_time on system_time
+for each row
+begin
+  update product 
+  set status = 'closed'
+  where status = 'underauction' and number_of_days <= :new.current_time;
+end;
+/
 
 
