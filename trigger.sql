5f0f031ccb27c33469d0840f4bc7fbcc5eaa5882
anb186 <42851849+anb186@users.noreply.github.com>
2018-11-19 09:26:25 -0500

    Update trigger.sql

--- trigger.sql
+++ trigger.sql
@@ -43,16 +43,78 @@ end;
 /
 
 
-CREATE OR REPLACE FUNCTION func_productCount; 
+--counts the number of products sold in the past (mon) months for specific categories (cat) 
+CREATE OR REPLACE FUNCTION func_productCount(mon INT, cat VARCHAR2)
+    RETURN INT
+IS       
+    currDate DATE;
+    prodSold INT;
+BEGIN
+    SELECT c_date
+    INTO currdate
+    FROM ourSysDATE
+    WHERE ROWNUM = 1
+    ORDER BY c_date DESC;
+    
+    SELECT COUNT(p.auction_id)
+    INTO prodSold
+    FROM Product p, BelongsTo b
+    WHERE b.category = cat
+    AND p.status = 'sold'
+    AND p.sell_date >= add_months(currDate, -mon);
+    
+    RETURN prodSold;
+END;
+/
+
+--counts the number of bids a specific user (user) has placed in the past (mon) months 
+CREATE OR REPLACE FUNCTION func_bidCount(mon INT, user VARCHAR2)
+    RETURN INT
+IS
+    currDate DATE;
+    numBids INT;
+BEGIN
+    SELECT  c_date
+    INTO currdate
+    FROM ourSysDATE
+    WHERE ROWNUM = 1
+    ORDER BY c_date DESC;
+    
+    SELECT COUNT(b.bidsn)
+    INTO numBids
+    FROM bidlog b, customer c
+    WHERE b.bidder = c.login
+    AND c.login = user 
+    AND b.bid_time >= add_months(currDate, -mon);
+
+    RETURN numBids;
+END;
+/
 
 
+--calculates the total dollar amount a specific user (user) has spent in the past (mon) months, 
+CREATE OR REPLACE FUNCTION func_buyingAmount(mon INT, user VARCHAR2)
+    RETURN INT
+IS
+    currDate DATE;
+    amountSpent INT;
+BEGIN
+        
+    SELECT  c_date
+    INTO currdate
+    FROM ourSysDATE
+    WHERE ROWNUM = 1
+    ORDER BY c_date DESC;
+    
+    SELECT SUM(p.amount)
+    INTO amountSpent
+    FROM product p
+    WHERE p.buyer = user 
+    AND p.sell_date >= add_months(currDate, -mon);
 
-CREATE OR REPLACE FUNCTION func_bidCount;
-
-
-
-CREATE OR REPLACE FUNCTION func_buyingAmount;
-
+    RETURN amountSpent;
+END;
+/
 
 
 CREATE OR REPLACE TRIGGER trig_closeAuctions
