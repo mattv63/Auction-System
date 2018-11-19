39467c0f867c77436e8e74a989bb23b0877d0ec5
Matthew Viego <mjv35@pitt.edu>
2018-11-18 19:23:22 -0500

    added procedure and trigger

--- trigger.sql
+++ trigger.sql
@@ -1,14 +1,37 @@
 --trigger, fucntions, procedures, etc
 
-CREATE OR REPLACE PROCEDURE proc_putProduct;
+CREATE OR REPLACE PROCEDURE proc_putProduct
+(
+    pAuction_ID out int,
+    pName in varchar2,
+    pDesciption in varchar2,
+    pSeller in varchar2,
+    pMin_price in int,
+    pNumber_of_days in int
+)
+is
+    pStart_date date;
+begin
+    select max(auction_ID) into pAuction_ID from Product;
+    pAuction_ID := pAuction_ID + 1;
+    
+    select max(c_date) into pStart_date from ourSysDate;
+    
+    insert into Product(auction_id, name, desciption, seller, start_date, min_price, number_of_days, status)
+    values(pAuction_ID, pName, pDescription, pSeller, pStart_date, pMin_price, pNumber_of_days, 'in auction');
+end proc_putProduct;
 
+-- When new bid is inserted, advances system time by five seconds.  
+CREATE OR REPLACE TRIGGER trig_bidTimeUpdate
+after insert on bidlog
+declare new_date date;
+begin
+    select(max(c_date) + (5/24/60/60)) into new_date from ourSysDATE;
+    update ourSysDATE
+    set c_date = to_date(new_date, 'DD-MON-YYYY/HH12:MI:SS:AM');
+end;
 
-
-CREATE OR REPLACE TRIGGER trig_bidTimeUpdate;
-
-
-
-CREATE OR REPLACE TRIGGER trig_updateHighBid;
+CREATE OR REPLACE TRIGGER trig_updateHighBid
 -- to do
 after insert on bidlog
 for each row
@@ -32,7 +55,7 @@ CREATE OR REPLACE FUNCTION func_buyingAmount;
 
 
 
-CREATE OR REPLACE TRIGGER trig_closeAuctions;
+CREATE OR REPLACE TRIGGER trig_closeAuctions
 -- to do
 after update of current_time on system_time
 for each row
